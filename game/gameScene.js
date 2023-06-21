5a5801dbf0270fd7be137b96a99dd65fc119aa88
grittyzavr <gritty1337@protonmail.com>
2023-06-22 01:54:02 +0300

    динозаврик падает!!! комментарии добавил

--- game/gameScene.js
+++ game/gameScene.js
@@ -5,12 +5,24 @@ const gameScene = new Phaser.Class({
     Phaser.Scene.call(this, { key: 'gameScene' });
   },
 
-  preload() {
-    // Загрузка ресурсов для игры
+  preload() { // опять же, в прилоаде ассеты для игры
+    this.load.image('background', 'https://i.postimg.cc/xnf9kMsf/background2.jpg?dl=1');
+    this.load.spritesheet('tHex', 'https://i.postimg.cc/7Pn95fxc/1-bu.png', {
+      frameWidth: 243,
+      frameHeight: 370,
+    });
+    this.load.image('ground', 'https://upload.wikimedia.org/wikipedia/commons/2/22/Chromium_T-Rex-offline-trex.png');
   },
 
   create() {
-    // Создание игровой сцены
+  // Add a background image
+    const backgroundImage = this.add.image(700, 250, 'background'); // тут надо придумать как сделать обои и землю бесконечными
+    const tHex = this.physics.add.sprite(100, 100, 'tHex'); // непосредственно чубрик наш.
+    tHex.setGravityY(800); // гравитация по вертикали для братца нашего
+    tHex.setScale(0.5); // масштабируем чубрика(он слишком здоровый в оригинале)
+    const ground = this.physics.add.staticImage(400, 720, 'ground'); // тут надо придумать как сделать обои и землю бесконечными
+    ground.setScale(2).refreshBody();
+    this.physics.add.collider(tHex, ground); // тут мы заставляем чубрика падать именно на землю.
   },
 
   update() {
--- index.js
+++ index.js
@@ -4,26 +4,33 @@ const config = {
   type: Phaser.AUTO,
   width: 1280,
   height: 720,
-  scene: [{ preload, create }, gameScene],
+  scene: [{ preload, create }, gameScene], // Передаем в конструктор массив сцен
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
-
+// функция preload отвечает за ассеты которые игра должна подтянуть для себя)
 function preload() {
   this.load.image('background', 'https://i.postimg.cc/xnf9kMsf/background2.jpg?dl=1');
 }
-
+// функция create - создает сцену
 function create() {
   this.add.image(700, 250, 'background');
 
   const playButton = this.add.text(460, 300, 'Новая игра', { fill: '#0f0' })
     .setInteractive()
     .on('pointerdown', startGame.bind(this)); // Вот здесь мы привязываем контекст
-
+  // тут пока ничего не делаем
   const leveChoiceButton = this.add.text(660, 300, 'Выбор уровня', { fill: '#0f0' })
     .setInteractive()
     .on('pointerdown', () => levelChoice());
-
+  // тут пока ничего не делаем
   const creditsButton = this.add.text(890, 300, 'Авторы', { fill: '#0f0' })
     .setInteractive()
     .on('pointerdown', () => credits());
@@ -35,12 +42,12 @@ function create() {
   }
 
   function levelChoice() {
-    console.log('Открытие настроек...');
-    // Здесь вы можете переключиться на другую сцену с настройками
+    console.log('Открытие выбора уровня...');
+    // тут пропишем переключение на сцену для уровней(если все-таки будем их делать)
   }
 
   function credits() {
-    console.log('Открытие настроек...');
-    // Здесь вы можете переключиться на другую сцену с настройками
+    console.log('Открытие титров...');
+    // тут пропишем переключение на сцену для титров
   }
 }
