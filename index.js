6d33c0bafb807b0226d2343ef48e1103bfb130a7
kwaz1k <petenka100@gmail.com>
2023-06-23 22:25:00 +0300

    Добавил кнопки и эффект при наведении

--- index.js
+++ index.js
@@ -18,22 +18,50 @@ const game = new Phaser.Game(config);
 // функция preload отвечает за ассеты которые игра должна подтянуть для себя)
 function preload() {
   this.load.image('background', 'https://i.postimg.cc/xnf9kMsf/background2.jpg?dl=1');
+  this.load.image('defaultPlayButtonImage', 'https://i.postimg.cc/BbdPBjZ6/play-Button-Image.png'); // обычное изображение
+  //this.load.image('shinePlayButtonImage', 'https://i.postimg.cc/zG6WNB6H/shine-Play-Button-Image-copy.png'); // изображение при наведении
+  this.load.image('defaultSelectButtonImage', 'https://i.postimg.cc/ncVgpLw4/select-Button-Image.png');
+  //this.load.image('shineSelectButtonImage', 'https://i.postimg.cc/0Qxz5Jfj/shine-Select-Button-Image-copy.png');
+  this.load.image('defaultAutorButtonImage', 'https://i.postimg.cc/PqfQ8LYy/autor-Button-Image.png');
+  //this.load.image('shineAutorButtonImage', 'https://i.postimg.cc/cCkCckNt/shine-Autor-Button-Image-copy.png');
 }
 // функция create - создает сцену
 function create() {
   this.add.image(700, 250, 'background');
 
-  const playButton = this.add.text(460, 300, 'Новая игра', { fill: '#0f0' })
+  const defaultPlayButtonImage = this.add.image(466, 360, 'defaultPlayButtonImage')
     .setInteractive()
     .on('pointerdown', startGame.bind(this)); // Вот здесь мы привязываем контекст
+    
+    defaultPlayButtonImage.on('pointerover', function() {
+      defaultPlayButtonImage.setScale(1.05); // Увеличение размера при наведении
+  });
+    
+    defaultPlayButtonImage.on('pointerout', function() {
+      defaultPlayButtonImage.setScale(1); // Возврат к обычному размеру при уходе указателя
+  });
   // тут пока ничего не делаем
-  const leveChoiceButton = this.add.text(660, 300, 'Выбор уровня', { fill: '#0f0' })
+  const leveChoiceButton = this.add.image(696, 360, 'defaultSelectButtonImage')
     .setInteractive()
     .on('pointerdown', () => levelChoice());
+    leveChoiceButton.on('pointerover', function() {
+      leveChoiceButton.setScale(1.05); // Увеличение размера при наведении
+  });
+    
+    leveChoiceButton.on('pointerout', function() {
+      leveChoiceButton.setScale(1); // Возврат к обычному размеру при уходе указателя
+  });
   // тут пока ничего не делаем
-  const creditsButton = this.add.text(890, 300, 'Авторы', { fill: '#0f0' })
+  const creditsButton = this.add.image(926, 360, 'defaultAutorButtonImage')
     .setInteractive()
     .on('pointerdown', () => credits());
+    creditsButton.on('pointerover', function() {
+      creditsButton.setScale(1.05); // Увеличение размера при наведении
+  });
+    
+    creditsButton.on('pointerout', function() {
+      creditsButton.setScale(1); // Возврат к обычному размеру при уходе указателя
+  });
 
   function startGame() {
     console.log('Игра начинается...');
