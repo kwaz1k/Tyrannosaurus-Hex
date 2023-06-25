7f0efdde924623db0c04b8428d4b3c2a160e65a5
YoungPunk21 <cryb849@gmail.com>
2023-06-25 23:13:20 +0300

    Text score

--- game/gameScene.js
+++ game/gameScene.js
@@ -5,7 +5,7 @@ const gameScene = new Phaser.Class({
     Phaser.Scene.call(this, { key: 'gameScene' });
   },
 
-  preload() { // опять же, в прилоаде ассеты для игры
+  preload() { // загрузка ассетов игры
     this.load.image('backgroundGame', 'https://i.postimg.cc/ncCJCFZ6/wallpaperbetter-com-3840x2160-1.jpg');
     this.load.spritesheet('tHex', 'https://i.postimg.cc/7Pn95fxc/1-bu.png', {
       frameWidth: 243,
@@ -15,27 +15,32 @@ const gameScene = new Phaser.Class({
   },
 
   create() {
-    const backgroundImage = this.add.image(640, 360, 'backgroundGame'); // тут надо придумать как сделать обои и землю бесконечными
-    const tHex = this.physics.add.sprite(100, 100, 'tHex'); // непосредственно чубрик наш.
-    tHex.setGravityY(800); // гравитация по вертикали для братца нашего
-    tHex.setScale(0.5); // масштабируем чубрика(он слишком здоровый в оригинале)
-    const ground = this.physics.add.staticImage(900, 800, 'ground'); // тут надо придумать как сделать обои и землю бесконечными
+    const backgroundImage = this.add.image(640, 360, 'backgroundGame'); // фоновое изображение
+    const tHex = this.physics.add.sprite(100, 100, 'tHex'); // персонаж
+    tHex.setGravityY(800); // гравитация по вертикали
+    tHex.setScale(0.5); // масштабирование персонажа
+    const ground = this.physics.add.staticImage(900, 800, 'ground'); // земля
     ground.setScale(2).refreshBody();
     this.physics.add.collider(tHex, ground);
-    const cursors = this.input.keyboard.createCursorKeys(); // объект содержащий стрелки и пробел
+    const cursors = this.input.keyboard.createCursorKeys(); // объект, содержащий клавиши управления
     const jump = () => {
       if (tHex.body.touching.down) {
         tHex.setVelocityY(-800);
         this.isRunning = true;
+        this.score += 10; // начисление 10 баллов за прыжок
+        this.scoreText.setText('Score: ' + this.score); // обновление текста счета
       }
     };
-    cursors.up.on('down', jump); // прыжок на стрелку вверх
-    cursors.space.on('down', jump); // прыжок на пробел
-    this.input.on('pointerdown', jump); // прыжок на нажатие экрана
-    this.speed = 5; // Adjust the running speed as needed
+    cursors.up.on('down', jump); // прыжок по стрелке вверх
+    cursors.space.on('down', jump); // прыжок по пробелу
+    this.input.on('pointerdown', jump); // прыжок по нажатию экрана
+    this.speed = 5; // скорость перемещения персонажа
     this.isRunning = false;
     this.cameras.main.startFollow(tHex);
-    // Update function to move the character forward
+    this.score = 0; // переменная для хранения текущего счета
+    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' }); // текст счета
+
+    // функция обновления, перемещающая персонажа вперед
     this.update = () => {
       if (this.isRunning) {
         tHex.x += this.speed;
@@ -47,8 +52,8 @@ const gameScene = new Phaser.Class({
   },
 
   update() {
-    // Обновление игровой сцены
+    // обновление игровой сцены
   },
 });
 
-export { gameScene };// тут мы заставляем чубрика падать именно на землю.
+export { gameScene };
