37e28492a05026cf8e6c2d003d8534f7e98d3802
YoungPunk21 <cryb849@gmail.com>
2023-06-26 20:58:52 +0300

    obstacle

--- game/gameScene.js
+++ game/gameScene.js
@@ -5,13 +5,18 @@ const gameScene = new Phaser.Class({
     Phaser.Scene.call(this, { key: 'gameScene' });
   },
 
-  preload() { // загрузка ассетов игры
+  preload() {
+    // Загрузка ассетов игры
     this.load.image('backgroundGame', 'https://i.postimg.cc/ncCJCFZ6/wallpaperbetter-com-3840x2160-1.jpg');
     this.load.spritesheet('tHex', 'https://i.postimg.cc/SsRRc797/spritesheet.png', {
       frameWidth: 243,
       frameHeight: 370,
     });
     this.load.image('ground', 'https://i.postimg.cc/8c9BWznw/ground.png');
+    this.load.image('obstacle1', 'https://i.postimg.cc/ZRfh2H7N/car.png');
+    this.load.image('obstacle2', 'https://i.postimg.cc/zXH12nnL/container.png');
+    this.load.image('obstacle3', 'https://i.postimg.cc/Zn9XgSyk/mahina.png');
+    this.load.image('obstacle4', 'https://i.postimg.cc/X7krqSzc/trash-ready.png');
   },
 
   create() {
@@ -25,16 +30,27 @@ const gameScene = new Phaser.Class({
       frames: this.anims.generateFrameNumbers('tHex', { start: 0, end: 0 }),
       frameRate: 10,
     });
+
     const { width, height } = this.sys.game.config;
-    this.backgroundImage = this.add.tileSprite(0, 0, this.sys.game.config.width, this.sys.game.config.height, 'backgroundGame').setOrigin(0, 0).setScrollFactor(0).setScale(1920 / width, 1200 / height);
+
+    // Задний фон игры
+    this.backgroundImage = this.add
+      .tileSprite(0, 0, this.sys.game.config.width, this.sys.game.config.height, 'backgroundGame')
+      .setOrigin(0, 0)
+      .setScrollFactor(0)
+      .setScale(1920 / width, 1200 / height);
+
     const tHex = this.physics.add.sprite(1080, this.sys.game.config.height - 200, 'tHex'); // персонаж
+
     this.grounds = this.physics.add.staticGroup();
     for (let i = 0; i < 3; i += 1) {
       const ground = this.grounds.create(1080 + i * 1080, this.sys.game.config.height, 'ground');
       this.physics.add.collider(tHex, ground);
     }
+
     tHex.setGravityY(800); // гравитация по вертикали
     tHex.setScale(0.5); // масштабирование персонажа
+
     const cursors = this.input.keyboard.createCursorKeys(); // объект, содержащий клавиши управления
     const jump = () => {
       if (tHex.body.touching.down) {
@@ -45,15 +61,34 @@ const gameScene = new Phaser.Class({
         tHex.anims.play('jump'); // текстура прыжка
       }
     };
+
     cursors.up.on('down', jump); // прыжок по стрелке вверх
     cursors.space.on('down', jump); // прыжок по пробелу
     this.input.on('pointerdown', jump); // прыжок по нажатию экрана
+
     this.speed = 5; // скорость перемещения персонажа
     this.isRunning = false;
+
     this.cameras.main.startFollow(tHex, true, 0.5, 0.5, 0, 150);
+
     this.score = 0; // переменная для хранения текущего счета
-    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff', fontFamily: 'MinecraftiaRegular' }); // текст счета
-    this.scoreText.setScrollFactor(0);
+    this.scoreText = this.add
+      .text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff', fontFamily: 'MinecraftiaRegular' }) // текст счета
+      .setScrollFactor(0);
+
+    this.obstacles = this.physics.add.group({ immovable: true }); // Препятствие и их логика
+
+    for (let i = 0; i < 4; i++) {
+      const obstacleNumber = Phaser.Math.Between(1, 4);
+      const obstacleX = Phaser.Math.Between(2000, 4000);
+      const obstacle = this.obstacles.create(obstacleX, this.sys.game.config.height - 300, `obstacle${obstacleNumber}`).setScale(0.5);
+      obstacle.setCollideWorldBounds(true);
+      obstacle.setVelocityX(-this.speed);
+      this.physics.add.collider(tHex, obstacle, () => {
+        this.scene.restart();
+      }, null, this);
+    }
+
     // функция обновления, перемещающая персонажа вперед
     this.update = () => {
       if (this.isRunning) {
@@ -66,9 +101,22 @@ const gameScene = new Phaser.Class({
       if (tHex.x > this.grounds.getChildren()[0].x + 1080) {
         // Удалить старую землю
         this.grounds.getChildren()[0].destroy();
+
         // Добавить новую землю
         const newGround = this.grounds.create(this.grounds.getChildren()[this.grounds.getChildren().length - 1].x + 1080, this.sys.game.config.height, 'ground');
         this.physics.add.collider(tHex, newGround);
+
+        //  новые препятствия
+        for (let i = 0; i < 4; i++) {
+          const obstacleNumber = Phaser.Math.Between(1, 4);
+          const obstacleX = Phaser.Math.Between(this.grounds.getChildren()[this.grounds.getChildren().length - 1].x + 2000, this.grounds.getChildren()[this.grounds.getChildren().length - 1].x + 4000);
+          const obstacle = this.obstacles.create(obstacleX, this.sys.game.config.height - 300, `obstacle${obstacleNumber}`).setScale(0.5);
+          obstacle.setCollideWorldBounds(true);
+          obstacle.setVelocityX(-this.speed);
+          this.physics.add.collider(tHex, obstacle, () => {
+            this.scene.restart();
+          }, null, this);
+        }
       }
     };
   },
