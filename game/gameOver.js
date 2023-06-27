0b29e7b6a6d4209bb772797045066215c413e80e
grittyzavr <gritty1337@protonmail.com>
2023-06-27 23:29:13 +0300

    no sound:(

--- game/gameOver.js
+++ game/gameOver.js
@@ -8,12 +8,9 @@ const gameOver = new Phaser.Class({
   preload() {
     this.load.image('playAgainButton', 'https://i.postimg.cc/pXnr4zVQ/4.png');
     this.load.image('gameOverPicture', 'https://i.postimg.cc/13rq7wMC/image.png');
-    this.load.audio('gameOverSound', 'https://archive.org/download/NIntendo-sounds/Mario%201%20-%20Game%20Over.ogg');
   },
 
   create() {
-    const sound = this.sound.add('gameOverSound');
-    sound.play();
     const { width } = this.sys.game.config;
     const { height } = this.sys.game.config;
     const background = this.add.image(0, 0, 'gameOverPicture').setOrigin(0, 0);
